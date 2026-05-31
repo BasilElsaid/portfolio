b20b8e572cdc1597ec49dfbfcb604f3a47570585
Basil Elsaid <basil_elsaid@outlook.com>
2026-05-31 14:09:35 +0200

    about page impostata

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,23 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  techStack = [
+    { name: 'Java', icon: 'assets/icons/java.svg' },
+    { name: 'TypeScript', icon: 'assets/icons/typescript.svg' },
+    { name: 'JavaScript', icon: 'assets/icons/javascript.svg' },
+    { name: 'HTML', icon: 'assets/icons/html5.svg' },
+    { name: 'CSS', icon: 'assets/icons/css3.svg' },
+    { name: 'Angular', icon: 'assets/icons/angular.svg' },
+    { name: 'Spring Boot', icon: 'assets/icons/springboot.svg' },
+    { name: 'NestJS', icon: 'assets/icons/nestjs.svg' },
+    { name: 'MySQL', icon: 'assets/icons/mysql.svg' },
+    { name: 'MongoDB', icon: 'assets/icons/mongodb.svg' },
+  ];
 }
