c9d7548f2c4fbcf87ab6f583b5002bb3b0b1823e
Basil Elsaid <basil_elsaid@outlook.com>
2026-06-01 12:39:38 +0200

    home page migliorata

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,8 +5,26 @@ import { RouterLink } from '@angular/router';
   selector: 'app-home',
   imports: [RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  typedText = '';
 
+  private fullText =
+    'Junior Backend-oriented developer focalizzato su API REST, architettura software e applicazioni web moderne.';
+
+  private index = 0;
+
+  ngOnInit() {
+    this.type();
+  }
+
+  type() {
+    if (this.index < this.fullText.length) {
+      this.typedText += this.fullText[this.index];
+      this.index++;
+
+      setTimeout(() => this.type(), 25); // velocità typing
+    }
+  }
 }
