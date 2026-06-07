e9366da49187cbb93550f46e62beae0f254e7a47
Basil Elsaid <basil_elsaid@outlook.com>
2026-06-07 13:32:07 +0200

    back to home bottone in tutte le pagine

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, RouterLink],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
